5bf4a88f35b6a336bc39c6e7e84d3732f9152e79
Yassine Sedrani <xmoroccan@users.noreply.github.com>
2021-05-07 16:26:14 +0000

    Apply fixes from StyleCI

--- src/MetaServiceProvider.php
+++ src/MetaServiceProvider.php
@@ -21,10 +21,12 @@ class MetaServiceProvider extends ServiceProvider
         }
 
         $this->publishes(
-        [
-            dirname(__DIR__).'/config/meta.php'  => config_path('meta.php'),
-            dirname(__DIR__).'/config/cache.php' => config_path('metacache.php'),
-        ], 'config');
+            [
+                dirname(__DIR__).'/config/meta.php'  => config_path('meta.php'),
+                dirname(__DIR__).'/config/cache.php' => config_path('metacache.php'),
+            ],
+            'config'
+        );
 
         Blade::directive('meta', function ($expr) {
             return '<?php echo \IMW\LaravelMeta\Meta::generate('.$expr.') ?>';
